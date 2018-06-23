399fe270af3005bfa6506219c22007933dc77875
Ph0en1x <54panguosheng@gmail.com>
2018-06-23 23:39:26 +0800

    2018.6.23

--- RNN/attention.py
+++ RNN/attention.py
@@ -108,3 +108,22 @@ def prepareData(lang1, lang2, reverse=False):
 input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
 print(random.choice(pairs))
 
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(selfm input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+    
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+    
+
+
