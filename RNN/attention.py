170bca9a5fdf359d19ce3997799593600b282a63
Ph0en1x <54panguosheng@gmail.com>
2018-07-05 22:00:38 +0800

    2018.7.5

--- RNN/attention.py
+++ RNN/attention.py
@@ -333,7 +333,26 @@ def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
         for ei in range(input_length):
             encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
             encoder_output[ei] += encoder_output[0, 0]
-            
 
 
-        
+        decoder_input = torch.tensor([[SOS_token]], device=device)
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+        return decoded_words, decoder_attentions[:di+1]
+
